acace3e9153f8a2550b3e3fa7b4b2225a2fe27c7
lluvz <2572502109@qq.com>
2023-04-07 18:12:54 +0800

    Child record its index in the vec

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub struct AutoVec<T> {
 #[derive(Debug)]
 pub struct AutoChild<T> {
     pub parent: *const AutoVec<T>,
+    pub index: usize,
     pub child: T,
 }
 impl<T> Deref for AutoChild<T> {
@@ -25,26 +26,23 @@ impl<T> AutoChild<T> {
     pub fn new(child: T) -> Pin<Box<Self>> {
         Box::pin(
             Self {
-                parent: 0 as _,
+                parent: 0 as _, index: 0,
                 child,
             }
         )
-    }
-    fn set_parent(&mut self, parent: *const AutoVec<T>) {
-        self.parent = parent;
     }
 }
 impl<T> Drop for AutoChild<T> {
     fn drop(&mut self) {
         if self.parent!= 0 as _ {
-            unsafe {(self.parent as *mut AutoVec<T>).as_mut()}.unwrap().called_remove(self as *const AutoChild<T>);
+            unsafe {(self.parent as *mut AutoVec<T>).as_mut()}.unwrap().called_remove(self);
         }
     }
 }
 impl<T> Drop for AutoVec<T> {
     fn drop(&mut self) {
         for i in &self.children {
-            unsafe {(*i as *mut AutoChild<T>).as_mut().unwrap()}.set_parent(0 as _);
+            unsafe {(*i as *mut AutoChild<T>).as_mut().unwrap()}.parent = 0 as _;
         }
     }
 }
@@ -55,24 +53,15 @@ impl<T> AutoVec<T> {
         }
     }
     pub fn add(&mut self, child: &mut AutoChild<T>) {
-        child.set_parent(self as *const _);
+        child.parent = self as *const _;
+        child.index = self.children.len();
         self.children.push(child as *const _);
     }
-    fn called_remove(&mut self, child: *const AutoChild<T>) {
-        for i in 0..self.children.len() {
-            if self.children[i] == child as *const AutoChild<T> {
-                self.children.swap_remove(i);
-                break
-            }
-        }
+    fn called_remove(&mut self, child: &AutoChild<T>) {
+        self.children.swap_remove(child.index);
     }
     pub fn remove(&mut self, child: &mut AutoChild<T>) {
-        for i in 0..self.children.len() {
-            if self.children[i] == child as *const _ {
-                self.children.swap_remove(i);
-                child.set_parent(0 as _);
-                break
-            }
-        }
+        self.children.swap_remove(child.index);
+        child.parent = 0 as _;
     }
 }
